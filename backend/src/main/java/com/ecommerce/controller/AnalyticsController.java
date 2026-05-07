8bf93c977261b33a1b763e3f3a2c35628faae098
Manikanth <manikanthetikyala174@gmail.com>
2026-05-07 12:24:03 +0530

    mani edited

--- backend/src/main/java/com/ecommerce/controller/AnalyticsController.java
+++ backend/src/main/java/com/ecommerce/controller/AnalyticsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
+// mani edited
 // hi mi name is mani
 // jai telangana
 @RestController
